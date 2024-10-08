48b8194b192fb9a03300acb37cf8d9cab9c655b2
Moaaz <moaaz.atik@gmail.com>
2024-10-08 16:32:26 +0300

    Cleaned up NoteViewModelTest and enhanced initializing its tests

--- app/src/test/java/com/thewhitewings/pouch/ui/NoteViewModelTest.kt
+++ app/src/test/java/com/thewhitewings/pouch/ui/NoteViewModelTest.kt
@@ -10,7 +10,6 @@ import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.ArgumentMatchers.anyInt
@@ -34,27 +33,6 @@ class NoteViewModelTest {
     // Test dispatcher for coroutines
     private val testDispatcher = UnconfinedTestDispatcher()
 
-    @Before
-    fun setUp() {
-        // Mock the repository
-        notesRepository = mock(NotesRepository::class.java)
-
-//        // Initialize SavedStateHandle without noteIdArg
-//        savedStateHandle = SavedStateHandle()
-//
-//        // Initialize ViewModel before each test, with a test dispatcher
-//        viewModel = NoteViewModel(savedStateHandle, notesRepository, testDispatcher)
-
-            // SavedStateHandle with noteIdArg
-            savedStateHandle = SavedStateHandle(mapOf(NoteDestination.noteIdArg to 1))
-
-            // Mock the repository response to return the mock note
-            whenever(notesRepository.getNoteById(1)).thenReturn(flowOf(mockNote1))
-
-            // Initialize ViewModel with the new SavedStateHandle
-            viewModel = NoteViewModel(savedStateHandle, notesRepository, testDispatcher)
-    }
-
     /**
      * Try to initialize NoteViewModel without passing in a noteIdArg. No repository call should be made and no ui state update should happen.
      * This case represents creating a new note.
@@ -66,27 +44,27 @@ class NoteViewModelTest {
         runTest {
             // When NoteViewModel is initialized, it calls initializeNote()
             // Since noteIdArg is not provided, no note should be loaded
+            initializeViewModelToCreateNewNote()
 
             // Then: Verify that the initial NoteUiState remains unchanged
             val expectedState = NoteViewModel.NoteUiState()
             val currentState = viewModel.noteUiState.value
             assertEquals(expectedState, currentState)
 
-            // Additionally, ensure that no interaction with notesRepository happens
+            // Ensure that no interaction with repository happens
             verify(notesRepository, never()).getNoteById(anyInt())
         }
 
+    /**
+     * Try to initialize NoteViewModel with a noteIdArg. A repository call should be made and ui state should be updated.
+     * This case represents updating an existing note.
+     * Case: noteIdArg is provided
+     * for [NoteViewModel.initializeNote]
+     */
     @Test
     fun `When initializing note and noteId is provided, get note from repository and update ui state`() =
         runTest {
-//            // SavedStateHandle with noteIdArg
-//            savedStateHandle = SavedStateHandle(mapOf(NoteDestination.noteIdArg to 1))
-//
-//            // Mock the repository response to return the mock note
-//            whenever(notesRepository.getNoteById(1)).thenReturn(flowOf(mockNote1))
-//
-//            // Initialize ViewModel with the new SavedStateHandle
-//            viewModel = NoteViewModel(savedStateHandle, notesRepository, testDispatcher)
+            initializeViewModelToUpdateExistingNote()
 
             // Collect the latest noteUiState value
             val currentState = viewModel.noteUiState.first()
@@ -98,4 +76,35 @@ class NoteViewModelTest {
             assertEquals(mockNote1, currentState.note)
         }
 
+
+    /**
+     * Initialize the ViewModel without passing in a [NoteDestination.noteIdArg] to create a new note.
+     */
+    private fun initializeViewModelToCreateNewNote() {
+        // Mock the repository
+        notesRepository = mock(NotesRepository::class.java)
+
+        // Initialize SavedStateHandle without noteIdArg
+        savedStateHandle = SavedStateHandle()
+
+        // Initialize ViewModel before each test, with a test dispatcher
+        viewModel = NoteViewModel(savedStateHandle, notesRepository, testDispatcher)
+    }
+
+    /**
+     * Initialize the ViewModel with a [NoteDestination.noteIdArg] to update an existing note.
+     */
+    private fun initializeViewModelToUpdateExistingNote() {
+        // Mock the repository
+        notesRepository = mock(NotesRepository::class.java)
+
+        // SavedStateHandle with noteIdArg
+        savedStateHandle = SavedStateHandle(mapOf(NoteDestination.noteIdArg to 1))
+
+        // Mock the repository response to return the mock note
+        whenever(notesRepository.getNoteById(1)).thenReturn(flowOf(mockNote1))
+
+        // Initialize ViewModel with the new SavedStateHandle
+        viewModel = NoteViewModel(savedStateHandle, notesRepository, testDispatcher)
+    }
 }
